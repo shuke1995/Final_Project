4d464a3c51834160de16c8ea4d018de99dec0844
joannake <shuangk2@illinois.edu>
2019-04-20 17:08:11 -0500

    read weather data in

--- test1.py
+++ test1.py
@@ -3,7 +3,12 @@
 import pandas as pd
 import numpy as np
 
+
+city_attributes = pd.read_csv('./historical-hourly-weather-data/city_attributes.csv')
+
 Humiditiy = pd.read_csv('./historical-hourly-weather-data/humidity.csv')
+Humiditiy['datetime'] = pd.to_datetime(Humiditiy['datetime'])
+Humiditiy.head()
 
 Pressure = pd.read_csv('./historical-hourly-weather-data/pressure.csv')
 
@@ -15,11 +20,32 @@ wind_direction = pd.read_csv('./historical-hourly-weather-data/wind_direction.cs
 
 wind_speed = pd.read_csv('./historical-hourly-weather-data/wind_speed.csv')
 
+chicago_crime = pd.read_csv('./Chicago_crime_2012-2017.csv')
+
+airpollution = pd.read_csv('./pollution_us_2000_2016.csv')
 
 
+airpollution.head()
+list(airpollution.columns.values)
+
+set(airpollution['City'])
+
+airpollution['Date Local']
+
+list(chicago_crime.columns.values)
+chicago_crime[['Primary Type', 'ID']].groupby(['Primary Type']).count()
+
+
+
+city_attributes.head()
 Humiditiy.head()
+
+Humiditiy['datetime']
+
 Pressure.head()
 Temperature.head()
 weather_description.head()
 wind_direction.head()
 wind_speed.head()
+
+
